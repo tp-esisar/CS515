08ab1564e91cbfe6017130d8a28be4c1ba72c823
guigeek <guigeek.b@live.fr>
2017-11-09 14:11:06 +0100

    traitement

--- tp2/src/traitement.ads
+++ tp2/src/traitement.ads
@@ -1,10 +1,11 @@
 with AdmInt;
+with Measure; use Measure;
 
 package Traitement is
    type T_Traitement is null record;
    
    function Moyenne(this: access T_Traitement; 
-                        liste: access AdmInt.SensorsMap.Map) 
-                        return Float;
+                        liste: access AdmInt.SensorMap.Map) 
+                        return T_Measure;
    
 end Traitement;
