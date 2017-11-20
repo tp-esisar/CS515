1afedee30e3b287a7505681e71400a7093b93aa7
guigeek <guigeek.b@live.fr>
2017-11-20 09:34:05 +0100

    pressures

--- tp3/src/pressuresensor.ads
+++ tp3/src/pressuresensor.ads
@@ -6,11 +6,6 @@ package PressureSensor is
 
    type T_PressureSensor is new T_AbstractPressureSensor with private;
    type T_PressureSensor_Access is access all T_PressureSensor'Class;
-   
-   package Constructor is
-      function Initialize(status: in Boolean; 
-                          pressure: in Float) return T_PressureSensor_Access;
-   end;
       
    overriding procedure simuleMeasure(this: access T_PressureSensor; 
                                       measure: in T_Measure);
